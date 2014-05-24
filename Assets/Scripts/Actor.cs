1ad3c09a95b9f2c6e80e8eb20b99decab05b3d2f
kitun <lukasz.jarzab13@gmail.com>
2014-05-24 12:45:06 +0200

    player controller

---
+++ Assets/Scripts/Actor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Actor : BaseScript {
+	public float actorSpeed;
+
+	protected CapsuleCollider capsuleCollider;
+
+
+	protected void Awake(){
+		capsuleCollider = (CapsuleCollider)collider;
+	}
+
+	protected bool IsGrounded(){
+		return Physics.Linecast(transform.position,transform.position - (Vector3.up * capsuleCollider.height/2.0f));
+	}
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : Actor  {
+
+	private float inputVerticalAxis = 0.0f;
+	private float inputHorizontalAxis = 0.0f;
+	private Vector3 desiredMovementSpeed = Vector3.zero;
+	private Vector3 movementDelta = Vector3.zero;
+
+	public float maxJumpHeight;
+
+
+
+	private void Update () {
+		inputHorizontalAxis = Input.GetAxis("Horizontal");
+		inputVerticalAxis = Input.GetAxis("Vertical");
+
+		if(Input.GetButtonDown("Jump")){
+			if(IsGrounded()){
+				Jump();
+			}
+		}
+	}
+
+	private void FixedUpdate() {
+		desiredMovementSpeed.x = inputHorizontalAxis * actorSpeed;
+		desiredMovementSpeed.z = 0.0f;
+
+		movementDelta = desiredMovementSpeed - rigidbody.velocity;
+		movementDelta.y = 0.0f;
+		movementDelta.z = 0.0f;
+
+		rigidbody.AddForce(movementDelta,ForceMode.VelocityChange);
+	}
+
+	private void Jump(){
+		StartCoroutine(JumpCoroutine());
+	}
+
+	private IEnumerator JumpCoroutine(){
+		float jumpStartYPosition = transform.position.y;
+		float jumpEndYPosition = jumpStartYPosition + maxJumpHeight;
+
+		while(Input.GetButton("Jump")){
+			if(transform.position.y < jumpEndYPosition){
+				rigidbody.AddForce(Vector3.up * rigidbody.mass * 20.0f);
+			}
+			else{
+				break;
+			}
+			yield return new WaitForFixedUpdate();
+		}
+	}
+
+
+}
