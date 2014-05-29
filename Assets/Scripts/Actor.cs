4d411dfd5c47d0198e14fae52eb249a5cc7c7f20
kitun <lukasz.jarzab13@gmail.com>
2014-05-29 22:51:05 +0200

    building character controller

--- Assets/Scripts/Actor.cs
+++ Assets/Scripts/Actor.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Actor : BaseScript {
 	public float actorSpeed;
 
+
 	protected CapsuleCollider capsuleCollider;
 
 
@@ -13,6 +14,16 @@ public class Actor : BaseScript {
 
 	protected bool IsGrounded(){
 		return Physics.Linecast(transform.position,transform.position - (Vector3.up * capsuleCollider.height/2.0f));
+	}
+
+	protected bool CanMoveInDirection(Vector3 direction){
+		Ray topRay = new Ray(transform.position + Vector3.up * 0.6f,direction);
+		Ray midRay = new Ray(transform.position, direction);
+		Ray bottomRay = new Ray(transform.position - Vector3.up * 0.6f,direction);
+		Debug.DrawRay(transform.position + Vector3.up * 0.6f,direction);
+		Debug.DrawRay(transform.position, direction);
+		Debug.DrawRay(transform.position - Vector3.up * 0.6f,direction);
+		return true;
 	}
 
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,19 +3,25 @@ using System.Collections;
 
 public class Player : Actor  {
 
-	private float inputVerticalAxis = 0.0f;
+
 	private float inputHorizontalAxis = 0.0f;
+	private float inputHorizontalAxisLastFrame = 0.0f;
+
+
 	private Vector3 desiredMovementSpeed = Vector3.zero;
 	private Vector3 movementDelta = Vector3.zero;
 
-	public float maxJumpHeight;
 
+	public float timeToReachMaxSpeed;
+	public float timeToRachFullStopFromMaxSpeed;
+	public AnimationCurve accelerationCurve;
 
+	private float currentDirectionMovementTime;
+	private float directionMovementTimeAtDirectionChange;
+	private float currentAcceleration;
 
 	private void Update () {
-		inputHorizontalAxis = Input.GetAxis("Horizontal");
-		inputVerticalAxis = Input.GetAxis("Vertical");
-
+		CalculateHorizontalMovement();
 		if(Input.GetButtonDown("Jump")){
 			if(IsGrounded()){
 				Jump();
@@ -23,34 +29,86 @@ public class Player : Actor  {
 		}
 	}
 
-	private void FixedUpdate() {
-		desiredMovementSpeed.x = inputHorizontalAxis * actorSpeed;
-		desiredMovementSpeed.z = 0.0f;
+	//this goes in normal update
+	private void CalculateHorizontalMovement(){
+		inputHorizontalAxis = Input.GetAxis("Horizontal");
 
-		movementDelta = desiredMovementSpeed - rigidbody.velocity;
+		if(inputHorizontalAxis != inputHorizontalAxisLastFrame){
+			if(inputHorizontalAxisLastFrame == 0.0f){
+
+				if(inputHorizontalAxis < 0.0f){
+					print ("started moving left");
+				}
+				else{
+					print ("started moving right");
+				}
+				//currentDirectionMovementTime *= -1.0f;
+			}
+			else{
+				if(inputHorizontalAxis == 0.0f){
+					print ("movement stopped");
+				}
+			}
+			inputHorizontalAxisLastFrame = inputHorizontalAxis;
+		}
+
+		if(inputHorizontalAxis == 0.0f){
+			if(currentDirectionMovementTime > 0.0f){
+				currentDirectionMovementTime -= Time.deltaTime;
+			}
+
+			else{
+				currentDirectionMovementTime = 0.0f;
+			}
+
+		}
+		else{
+			if(currentDirectionMovementTime < timeToReachMaxSpeed){
+				currentDirectionMovementTime += Time.deltaTime;
+			}
+			else{
+				currentDirectionMovementTime = timeToReachMaxSpeed;
+			}
+		}
+
+		//currentAcceleration =  accelerationCurve.Evaluate(currentDirectionMovementTime/timeToReachMaxSpeed); //.tutej
+		currentAcceleration = currentDirectionMovementTime/timeToReachMaxSpeed;
+
+
+		desiredMovementSpeed.x = inputHorizontalAxis * actorSpeed * currentAcceleration;
+		desiredMovementSpeed.z = 0.0f;
+		movementDelta = (desiredMovementSpeed - rigidbody.velocity);
 		movementDelta.y = 0.0f;
 		movementDelta.z = 0.0f;
+		CanMoveInDirection(desiredMovementSpeed/actorSpeed * 0.6f);
+	}
 
+
+
+	//this goes in fixed update
+	private void ApplyHorizontalMovement(){
 		rigidbody.AddForce(movementDelta,ForceMode.VelocityChange);
 	}
 
-	private void Jump(){
-		StartCoroutine(JumpCoroutine());
+	private void FixedUpdate() {
+		ApplyHorizontalMovement();
 	}
 
-	private IEnumerator JumpCoroutine(){
-		float jumpStartYPosition = transform.position.y;
-		float jumpEndYPosition = jumpStartYPosition + maxJumpHeight;
+	private void Jump(){
+		rigidbody.AddForce(Vector3.up*1000.0f,ForceMode.Impulse);
 
-		while(Input.GetButton("Jump")){
-			if(transform.position.y < jumpEndYPosition){
-				rigidbody.AddForce(Vector3.up * rigidbody.mass * 20.0f);
-			}
-			else{
-				break;
-			}
-			yield return new WaitForFixedUpdate();
-		}
+	}
+
+
+
+	private void OnGUI(){
+		ShowDebugData("current direction movement time",currentDirectionMovementTime);
+		ShowDebugData("current horizontal velocity",rigidbody.velocity.x);
+		ShowDebugData("current acceleration",currentAcceleration);
+	}
+
+	private void ShowDebugData(string propertyName, object propertyValue){
+		GUILayout.Label(propertyName + " : " + propertyValue);
 	}
 
 
