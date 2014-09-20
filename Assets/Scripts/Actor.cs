eda2e80d5be7e8e3a1a05d6b992987e3fd140c99
kitun <lukasz.jarzab13@gmail.com>
2014-09-20 15:59:18 +0200

    run'n'jump

--- Assets/Scripts/Actor.cs
+++ Assets/Scripts/Actor.cs
@@ -3,38 +3,44 @@ using System.Collections;
 
 public class Actor : BaseScript {
 
-	public enum EnumDirection{LEFT, RIGHT, IDLE};
+
 
 
 	public float actorSpeed;
 
-
-	protected EnumDirection currentDirection;
-
 	protected CapsuleCollider capsuleCollider;
-
 
 	protected void Awake(){
 		capsuleCollider = (CapsuleCollider)collider;
-		currentDirection = EnumDirection.IDLE;
 	}
 
 	protected bool IsGrounded(){
-		return Physics.Linecast(transform.position,transform.position - (Vector3.up * capsuleCollider.height/2.0f));
+		return Physics.Linecast(transform.position + capsuleCollider.center ,(transform.position + capsuleCollider.center) - (Vector3.up * capsuleCollider.height/2.0f));
 	}
 
 	protected bool CanMoveInDirection(Vector3 direction){
-		Ray topRay = new Ray(transform.position + Vector3.up * 0.6f,direction);
-		Ray midRay = new Ray(transform.position, direction);
-		Ray bottomRay = new Ray(transform.position - Vector3.up * 0.6f,direction);
-		Debug.DrawRay(transform.position + Vector3.up * 0.6f,direction);
-		Debug.DrawRay(transform.position, direction);
-		Debug.DrawRay(transform.position - Vector3.up * 0.6f,direction);
-		return true;
+		Ray topRay = new Ray(transform.position + capsuleCollider.center + Vector3.up * 0.6f,direction);
+		Ray midRay = new Ray(transform.position + capsuleCollider.center, direction);
+		Ray bottomRay = new Ray(transform.position + capsuleCollider.center - Vector3.up * 0.6f,direction);
+		Debug.DrawRay(transform.position + capsuleCollider.center + Vector3.up * 0.6f,direction);
+		Debug.DrawRay(transform.position + capsuleCollider.center, direction);
+		Debug.DrawRay(transform.position + capsuleCollider.center - Vector3.up * 0.6f,direction);
+		float length = capsuleCollider.radius;
+		return !Physics.Raycast(topRay,length) && !Physics.Raycast(midRay,length) && !Physics.Raycast(bottomRay,length);
 	}
 
-	protected virtual void setCurrentDirection(EnumDirection newDirection){
-		currentDirection = newDirection;
+	protected void SetActorVerticalVelocity(float verticalVelocity){
+		Vector3 tmp = rigidbody.velocity;
+		tmp.y = verticalVelocity;
+		rigidbody.velocity = tmp;
 	}
+	
+	protected void SetActorHorizontalVelocity(float horizontalVelocity){
+		Vector3 tmp = rigidbody.velocity;
+		tmp.x = horizontalVelocity;
+		rigidbody.velocity = tmp;
+	}
+
+
 
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,9 +8,11 @@ public class Player : Actor  {
 	private const float HORIZONTAL_INPUT_RANGE = 2.0f;
 
 
+
 	public float timeToReachMaxSpeed;
-	public float initialJumpForce;
-	public float jumpForceDampingRatio;
+	public float jumpHeight;
+	public float minJumpThreshold;
+	public Animator playerAnimator;
 
 
 	private float inputHorizontalAxis = 0.0f;
@@ -21,21 +23,32 @@ public class Player : Actor  {
 	private Vector3 desiredMovementSpeed = Vector3.zero;
 	private Vector3 movementDelta = Vector3.zero;
 
+	private bool isAttacking;
 
 
 	private void Awake(){
 		base.Awake();
 		smoothingSpeed = HORIZONTAL_INPUT_RANGE/timeToReachMaxSpeed;
-
+		isAttacking = false;
 	}
 
 	private void Update () {
 		HandleInput();
 		SmoothHorizontalAxis();
 		CalculateHorizontalMovement();
+		RotateCharacter();
+		HandleAnimations();
 	}
 
-	//this goes in normal update
+	private void HandleAnimations(){
+		playerAnimator.SetFloat("Blend",Mathf.Abs(rigidbody.velocity.x)/actorSpeed);
+		playerAnimator.SetBool("Grounded",IsGrounded());
+	}
+
+	private void FixedUpdate() {
+		ApplyHorizontalMovement();
+	}
+
 	private void CalculateHorizontalMovement(){
 		desiredMovementSpeed.x = smoothHorizontalAxis * actorSpeed;
 		movementDelta = (desiredMovementSpeed - rigidbody.velocity);
@@ -43,14 +56,10 @@ public class Player : Actor  {
 		movementDelta.z = 0.0f;
 	}
 
-
-
 	private void SmoothHorizontalAxis(){
-
 		if((Mathf.Abs((smoothHorizontalAxis+HORIZONTAL_INPUT_RANGE) - (inputHorizontalAxis+HORIZONTAL_INPUT_RANGE)) > Time.smoothDeltaTime)){
 			if(smoothHorizontalAxis > inputHorizontalAxis){
 				smoothHorizontalAxis -= Time.smoothDeltaTime * smoothingSpeed;
-
 			}
 			else if(smoothHorizontalAxis < inputHorizontalAxis){
 				smoothHorizontalAxis += Time.smoothDeltaTime * smoothingSpeed;
@@ -62,29 +71,49 @@ public class Player : Actor  {
 	}
 
 	private void HandleInput(){
-		inputHorizontalAxis = Input.GetAxisRaw("Horizontal");
+		if(!isAttacking){
+			if(CanMoveInDirection(new Vector3(Input.GetAxisRaw("Horizontal"),0,0))){
+				inputHorizontalAxis = Input.GetAxisRaw("Horizontal");
+			}
+			else{
+				inputHorizontalAxis = 0.0f;
+				SetActorHorizontalVelocity(0.0f);
+			}
+		}
+		else{
+			inputHorizontalAxis = 0.0f;
+			SetActorHorizontalVelocity(0.0f);
+		}
+
+
 
 		if(Input.GetButtonDown("Jump")){
 			if(IsGrounded()){
 				Jump();
 			}
 		}
-	}
 
-	protected void setCurrentDirection(EnumDirection newDirection){
-		if(currentDirection != newDirection){
-			currentDirection = newDirection;
+		if(Input.GetButtonDown("Fire1") && IsGrounded()){
+			isAttacking = true;
+			playerAnimator.SetTrigger("Attack Trigger");
 		}
 	}
 
+	private void RotateCharacter(){
+		if(Mathf.Abs(inputHorizontalAxis) > 0.1f){
+			if(inputHorizontalAxis < 0.0f){
+				transform.eulerAngles = new Vector3(0.0f,270.0f,0.0f);
+			}
+			else{
+				transform.eulerAngles = new Vector3(0.0f,90.0f,0.0f);
+			}
+		}
 
-	//this goes in fixed update
-	private void ApplyHorizontalMovement(){
-		rigidbody.AddForce(movementDelta,ForceMode.VelocityChange);
+
 	}
 
-	private void FixedUpdate() {
-		ApplyHorizontalMovement();
+	private void ApplyHorizontalMovement(){
+		rigidbody.AddForce(movementDelta,ForceMode.VelocityChange);
 	}
 
 	private void Jump(){
@@ -92,27 +121,38 @@ public class Player : Actor  {
 	}
 
 	private IEnumerator JumpCoroutine(){
-		float currentJumpForce = initialJumpForce;
 
-		while(Input.GetButton("Jump")){
-			rigidbody.AddForce(Vector3.up * currentJumpForce);
-			currentJumpForce *= jumpForceDampingRatio;
+		float initialVelocity = Mathf.Sqrt(jumpHeight * -2.0f * Physics.gravity.y);
+		rigidbody.AddForce(initialVelocity*Vector3.up,ForceMode.VelocityChange);
+
+		yield return new WaitForSeconds(minJumpThreshold);
+
+		while(rigidbody.velocity.y >= 0.0f){
+			if(!Input.GetButton("Jump")){
+				SetActorVerticalVelocity(rigidbody.velocity.y * 0.90f);
+			}
 			yield return new WaitForEndOfFrame();
 		}
 	}
 
 
 
-	private void OnGUI(){
 
+
+	private void OnGUI(){
 		ShowDebugData("current horizontal velocity",rigidbody.velocity.x);
 		ShowDebugData("current horizontal axis",inputHorizontalAxis);
 		ShowDebugData("current horizontal axis",smoothHorizontalAxis);
+		ShowDebugData("current vertical velocity",rigidbody.velocity.y);
 	}
 
 	private void ShowDebugData(string propertyName, object propertyValue){
 		GUILayout.Label(propertyName + " : " + propertyValue);
 	}
 
+	public void AttackFinished(){
+		print ("finisz");
+		isAttacking = false;
+	}
 
 }
